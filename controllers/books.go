9aa497702ee9aebc834aca275079b5075c9fae3d
Ravi Wu <raviwu@gmail.com>
2020-04-24 16:23:14 +0800

    Add update and delete endpoints

--- controllers/books.go
+++ controllers/books.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,11 @@ import (
 type CreateBookInput struct {
 	Title  string `json:"title" binding:"required"`
 	Author string `json:"author" binding:"required"`
+}
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
 
 func CreateBook(c *gin.Context) {
@@ -49,4 +55,39 @@ func FindBook(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func UpdateBook(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	var book models.Book
+	if err := db.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Record not found"})
+		return
+	}
+
+	// Validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.Model(&book).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	var book models.Book
+	if err := db.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		fmt.Printf("request book id [%s] does not exist", c.Param("id"))
+		c.AbortWithStatus(http.StatusOK)
+		return
+	}
+
+	db.Delete(&book)
+	c.AbortWithStatus(http.StatusOK)
 }
